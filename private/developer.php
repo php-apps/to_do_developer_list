ec5ed2d94320df34c63258d26734824c38b3de24
Vesna <veca1710@gmail.com>
2020-05-23 21:13:31 +0200

    Review changes

--- private/developer.php
+++ private/developer.php
@@ -1,6 +1,6 @@
 <?php
 require_once("config.php");
-// $user_id = $_SESSION['user_id']; //That's your developer ID
+//$user_id = $_SESSION['user_id']; //That's your admin ID
 $database = new Database();
 $db = $database->connect();
 $user = new User($db);
