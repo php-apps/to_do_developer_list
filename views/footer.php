67a6b9534b255f77a6c2b169796a3c256800b728
Vesna1992 <32514831+Vesna1992@users.noreply.github.com>
2020-06-10 12:50:23 +0200

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -6,8 +6,6 @@
             <a href="slack.com" target="_blank"><i class="fab fa-slack"></i></a>
     	</div>
         <p class="text">Copyright 2020</p>
-</footer>
-<script type="text/javascript" src="http://localhost/to_do_developer_list/js/collapsible.js"></script>
-<script type="text/javascript" src="http://localhost/to_do_developer_list/js/openCloseForm.js"></script>      
+</footer>   
 </body>
 </html>
